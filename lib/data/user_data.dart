224e0f384aef4d8599634c6af66f6fb48b707f66
Vinícius <marcosvhd1@gmail.com>
2024-01-05 00:04:47 -0300

    criação da classe de user data

---
+++ lib/data/user_data.dart
@@ -0,0 +1,52 @@
+class UserData {
+  static const name = "Marcos Vinícius Hellmann Delfino";
+  static const avatarUrl = "https://avatars.githubusercontent.com/u/56443755?v=4";
+  static const summary = "Desenvolvedor web e mobile focado em construir produtos com atenção extra aos detalhes.";
+
+  static const mapUrl = "https://www.google.com/maps/place/28%C2%B015'53.5%22S+49%C2%B001'13.6%22W/@-28.2699516,-49.0110314,15z/data=!4m4!3m3!8m2!3d-28.264867!4d-49.020449?entry=ttu";
+  static const mail = "marcosvhd1@gmail.com";
+  static const whatsapp = "5548996351582";
+  static const githubUrl = "https://github.com/marcosvhd1";
+  static const linkedinUrl = "https://www.linkedin.com/in/marcos-hellmann-2776b6229/";
+
+  static const about = "Desenvolvedor de software experiente com habilidades em JavaScript, Flutter e PHP. Experiência em desenvolvimento de aplicativos móveis e web, bem como em soluções de back-end. Meu objetivo é trabalhar em uma empresa inovadora onde eu possa aplicar minhas habilidades de programação para criar soluções de alta qualidade, contribuindo para o sucesso da empresa.";
+
+  static const work = [
+    {
+      'company': "Cubo Sistemas",
+      'title': "Help Desk",
+      'start': "Julho 2020",
+      'end': "Março 2022",
+      'description': "Criar tutorias e manuais para facilitar o uso do sistema, além de resolução de problemas, com foco na satisfação dos clientes.",
+    },
+    {
+      'company': "Cubo Sistemas",
+      'title': "Desenvolvedor",
+      'start': "Março 2022",
+      'end': "Dezembro 2023",
+      'description': "Desenvolvimento de software utilizando as linguagens de programação JavaScript, Flutter e PHP, comunicando com bancos de dados SQL e NoSQL."
+        "\n\n1. Sistema em React para emissão de notas fiscais eletrônicas. Para esse projeto utilizei a lib Chakra UI para o front-end e o framework AdonisJS para back-end. A comunicação com a SEFAZ foi feita em back-end PHP pela lib SPEDNFE. "
+        "\n\n2. Sistema em Flutter de apoio para auxiliar na prestação de serviço e venda em atendimentos externos. O app utiliza o banco SQFLite para salvar as informações offline e quando há acesso a internet, sincroniza as informações pelo Firebase RTDB.",
+    },
+  ];
+
+  static const education = [
+    {
+      'school': "Universidade do Sul de Santa Catarina",
+      'degree': "Bacharelado em Ciência da Computação",
+      'start': "2017",
+      'end': "2022",
+    },
+  ];
+
+  static const skills = [
+    "Flutter",
+    "Dart",
+    "React",
+    "Node.js",
+    "JavaScript",
+    "TypeScript",
+    "PHP",
+    "SQL",
+  ];
+}
